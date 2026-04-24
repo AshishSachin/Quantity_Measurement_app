ce63fa211dd7567f4d5277693d28b8e910ce1b5b
AshishSachin <nayakharsh04@gmail.com>
2026-04-24 08:51:23 +0530

    UC2

--- Quantity_measurement_app/src/Quantity_measurement_app.java
+++ Quantity_measurement_app/src/Quantity_measurement_app.java
@@ -1,5 +1,6 @@
 public class Quantity_measurement_app {
 
+    // Feet Class
     static class Feet {
         private final double value;
 
@@ -20,10 +21,47 @@ public class Quantity_measurement_app {
         }
     }
 
-    public static void main(String[] args) {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
+    // Inches Class
+    static class Inches {
+        private final double value;
 
-        System.out.println("Equal: " + f1.equals(f2));
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+
+            Inches inches = (Inches) obj;
+
+            return Double.compare(this.value, inches.value) == 0;
+        }
+    }
+
+    // Static method for Feet comparison
+    public static boolean compareFeet(double a, double b) {
+        Feet f1 = new Feet(a);
+        Feet f2 = new Feet(b);
+        return f1.equals(f2);
+    }
+
+    // Static method for Inches comparison
+    public static boolean compareInches(double a, double b) {
+        Inches i1 = new Inches(a);
+        Inches i2 = new Inches(b);
+        return i1.equals(i2);
+    }
+
+    public static void main(String[] args) {
+
+        // Feet comparison
+        System.out.println("Feet Equal: " + compareFeet(1.0, 1.0));
+
+        // Inches comparison
+        System.out.println("Inches Equal: " + compareInches(1.0, 1.0));
     }
 }
--- Quantity_measurement_app/src/Quantity_measurement_appTest.java
+++ Quantity_measurement_app/src/Quantity_measurement_appTest.java
@@ -4,39 +4,46 @@ import static org.junit.jupiter.api.Assertions.*;
 class Quantity_measurement_appTest {
 
     @Test
-    void testEquality_SameValue() {
-        Quantity_measurement_app.Feet f1 = new Quantity_measurement_app.Feet(1.0);
-        Quantity_measurement_app.Feet f2 = new Quantity_measurement_app.Feet(1.0);
-
-        assertTrue(f1.equals(f2));
+    void testFeetEquality_SameValue() {
+        assertTrue(Quantity_measurement_app.compareFeet(1.0, 1.0));
     }
 
     @Test
-    void testEquality_DifferentValue() {
-        Quantity_measurement_app.Feet f1 = new Quantity_measurement_app.Feet(1.0);
-        Quantity_measurement_app.Feet f2 = new Quantity_measurement_app.Feet(2.0);
-
-        assertFalse(f1.equals(f2));
+    void testFeetEquality_DifferentValue() {
+        assertFalse(Quantity_measurement_app.compareFeet(1.0, 2.0));
     }
 
     @Test
-    void testEquality_NullComparison() {
-        Quantity_measurement_app.Feet f1 = new Quantity_measurement_app.Feet(1.0);
-
-        assertFalse(f1.equals(null));
+    void testInchesEquality_SameValue() {
+        assertTrue(Quantity_measurement_app.compareInches(1.0, 1.0));
     }
 
     @Test
-    void testEquality_SameReference() {
-        Quantity_measurement_app.Feet f1 = new Quantity_measurement_app.Feet(1.0);
-
-        assertTrue(f1.equals(f1));
+    void testInchesEquality_DifferentValue() {
+        assertFalse(Quantity_measurement_app.compareInches(1.0, 2.0));
     }
 
     @Test
-    void testEquality_DifferentType() {
-        Quantity_measurement_app.Feet f1 = new Quantity_measurement_app.Feet(1.0);
+    void testFeet_NullComparison() {
+        Quantity_measurement_app.Feet f = new Quantity_measurement_app.Feet(1.0);
+        assertFalse(f.equals(null));
+    }
 
-        assertFalse(f1.equals("1.0"));
+    @Test
+    void testInches_NullComparison() {
+        Quantity_measurement_app.Inches i = new Quantity_measurement_app.Inches(1.0);
+        assertFalse(i.equals(null));
+    }
+
+    @Test
+    void testSameReference() {
+        Quantity_measurement_app.Feet f = new Quantity_measurement_app.Feet(1.0);
+        assertTrue(f.equals(f));
+    }
+
+    @Test
+    void testDifferentType() {
+        Quantity_measurement_app.Feet f = new Quantity_measurement_app.Feet(1.0);
+        assertFalse(f.equals("1.0"));
     }
 }
